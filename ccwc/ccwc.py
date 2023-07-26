990b1b01ffa0544558a058940e600ecefbb14b22
unknown <kepmiogokimiebi@gmail.com>
2023-07-26 14:08:53 +0200

    Add Byte Counter Tool with Argument Parsing

--- ccwc/ccwc.py
+++ ccwc/ccwc.py
@@ -1,3 +1,4 @@
+import argparse
 from abc import ABC, abstractmethod
 
 
@@ -7,12 +8,36 @@ class Counter(ABC):
         pass
 
 
+class ByteCounter(Counter):
+    def count(self, file_contents):
+        byte_count = 0
+        try:
+            with open(file_contents, 'rb') as file:
+                byte_data = file.read()
+                byte_count = len(byte_data)
+        except FileNotFoundError:
+            print(f"Error: File not found: {file_contents}")
+        return byte_count
+
+
 class CLI:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="Byte Counter Tool")
+        self.parser.add_argument("-c", nargs='?', const="-", default=None, help="File to count bytes in")
+
     def parse_args(self):
-        pass
+        args = self.parser.parse_args()
+        return args
 
     def run(self):
-        pass
+        args = self.parse_args()
+        arg_provided_by_user = any(val is not None for val in vars(args).values())
+
+        if arg_provided_by_user:
+            if args.c is not None:
+                byte_counter = ByteCounter()
+                total_bytes = byte_counter.count(args.c)
+                print(f"Number of bytes: {total_bytes}")
 
 
 if __name__ == "__main__":
