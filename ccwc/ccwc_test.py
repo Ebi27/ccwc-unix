400b6d34e4f59dd53f7309e264467fc83f2d75a3
unknown <kepmiogokimiebi@gmail.com>
2023-07-30 20:56:28 +0200

    Add test_valid_and_invalid_file_names to ccwc_test.py.

--- ccwc/ccwc_test.py
+++ ccwc/ccwc_test.py
@@ -20,7 +20,8 @@ def test_input_redirection():
         file_contents = file.read().strip()
 
     # Use input redirection to pass the file contents as input to ccwc.py
-    process = subprocess.Popen(['python', 'ccwc.py', '-l'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    process = subprocess.Popen(['python', 'ccwc.py', '-l'], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, text=True)
     stdout, stderr = process.communicate(input=file_contents)
 
     # Print the stdout and stderr for debugging
@@ -40,7 +41,8 @@ def test_standard_input_pipes():
     input_data = "The quick brown fox jumps over the lazy dog.\nThis is a test input.\n"
 
     # Use input redirection to pass the input_data to ccwc.py via standard input
-    process = subprocess.Popen(['python', 'ccwc.py', '-l'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    process = subprocess.Popen(['python', 'ccwc.py', '-l'], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, text=True)
     stdout, stderr = process.communicate(input=input_data)
 
     # Debugging in process
@@ -53,5 +55,42 @@ def test_standard_input_pipes():
 
 
 def test_valid_and_invalid_file_names():
-    # Test providing valid and invalid file names with different count options
-    pass
+    # Test valid file name with '-c' option (character count)
+    process = subprocess.Popen(['python', 'ccwc.py', '-c', 'test_sample.txt'], stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, text=True)
+    stdout, stderr = process.communicate()
+    assert process.returncode == 0
+    assert stdout.strip() == "Number of characters: 127"
+
+    # Test valid file name with '-l' option (line count)
+    process = subprocess.Popen(['python', 'ccwc.py', '-l', 'test_sample.txt'], stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, text=True)
+    stdout, stderr = process.communicate()
+    assert process.returncode == 0
+    assert stdout.strip() == "Number of lines: 2"
+
+    # Test invalid file name with '-c' option
+    process = subprocess.Popen(['python', 'ccwc.py', '-c', 'nonexistent_file.txt'], stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, text=True)
+    stdout, stderr = process.communicate()
+    assert process.returncode != 0
+    assert "Error: File 'nonexistent_file.txt' not found." in stderr
+
+    # Test invalid file name with '-l' option
+    process = subprocess.Popen(['python', 'ccwc.py', '-l', 'nonexistent_file.txt'], stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, text=True)
+    stdout, stderr = process.communicate()
+    assert process.returncode != 0
+    assert "Error: File 'nonexistent_file.txt' not found." in stderr
+
+    # Test providing no file name with '-c' option
+    process = subprocess.Popen(['python', 'ccwc.py', '-c'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    stdout, stderr = process.communicate()
+    assert process.returncode != 0
+    assert "Error: Please provide a valid file name." in stderr
+
+    # Test providing no file name with '-l' option
+    process = subprocess.Popen(['python', 'ccwc.py', '-l'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    stdout, stderr = process.communicate()
+    assert process.returncode != 0
+    assert "Error: Please provide a valid file name." in stderr
